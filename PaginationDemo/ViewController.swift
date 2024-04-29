77d9f6fef2f9636043dc5a1862a1571c81506cf9
Nitu Kumari <nitu.kumari@bluepi.in>
2024-04-29 18:46:37 +0530

    Initial Commit

---
+++ PaginationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaginationDemo
+//
+//  Created by Nitu Kumari on 29/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
